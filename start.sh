5b94da981abe4b4d240b5790bab595c4004dca9e
yangwu <yang.wu@igenetech.com>
2019-08-08 20:26:18 +0800

    Add indentation

--- start.sh
+++ start.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 ./parser parse \
--inputpath=/public/home/link/chart \
--sheet=1 \
--rowstart=2 \
--rowend=-1 \
--columns=1,2,3,4,5,9,11 \
--outputtype=txt \
--outputpath=output/ \
--remotepath=/root/upload \
--transfer=true \
--hostkey="***REMOVED***" \
--username=root \
--password=***REMOVED*** \
--watch=true \
--interval=5
+    -inputpath=/public/home/link/chart \
+    -sheet=1 \
+    -rowstart=2 \
+    -rowend=-1 \
+    -columns=1,2,3,4,5,9,11 \
+    -outputtype=txt \
+    -outputpath=output/ \
+    -remotepath=/root/upload \
+    -transfer=true \
+    -hostkey="***REMOVED***" \
+    -username=root \
+    -password=***REMOVED*** \
+    -watch=true \
+    -interval=5
