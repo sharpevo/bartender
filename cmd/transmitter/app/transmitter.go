83f8460112735171a2bd41a9459b8c1ca814b433
yangwu <yang.wu@igenetech.com>
2019-08-13 16:17:56 +0800

    change code layout

--- cmd/transmitter/app/transmitter.go
+++ cmd/transmitter/app/transmitter.go
@@ -3,16 +3,13 @@ package app
 import (
 	commonOptions "automation/cmd/options"
 	"automation/cmd/transmitter/options"
-	"regexp"
-	//"automation/parser/internal/fsop"
 	"automation/pkg/sshtrans"
 	"automation/pkg/watchrecur"
 	"flag"
-	//"fmt"
 	"github.com/sirupsen/logrus"
-	//"log"
 	"os"
 	"path/filepath"
+	"regexp"
 )
 
 func NewTransmitterCommand() {
@@ -21,83 +18,78 @@ func NewTransmitterCommand() {
 	serverOptions := commonOptions.AttachServerOptions(flag.CommandLine)
 	transferOptions := options.AttachTransferOptions(flag.CommandLine)
 	flag.Parse()
-	if flag.Parsed() {
-		switch logOptions.Level {
-		case "debug":
-			logrus.SetLevel(logrus.DebugLevel)
-		default:
-			logrus.SetLevel(logrus.InfoLevel)
-		}
+	switch logOptions.Level {
+	case "debug":
+		logrus.SetLevel(logrus.DebugLevel)
+	default:
+		logrus.SetLevel(logrus.InfoLevel)
+	}
+	logrus.WithFields(logrus.Fields{
+		"logOptions":      commonOptions.Debug(logOptions),
+		"watchOptions":    commonOptions.Debug(watchOptions),
+		"serverOptions":   commonOptions.Debug(serverOptions),
+		"transferOptions": commonOptions.Debug(transferOptions),
+	}).Debug("LOG")
+	r, err := regexp.Compile(watchOptions.FileNamePattern)
+	if err != nil {
 		logrus.WithFields(logrus.Fields{
-			"logOptions":      commonOptions.Debug(logOptions),
-			"watchOptions":    commonOptions.Debug(watchOptions),
-			"serverOptions":   commonOptions.Debug(serverOptions),
-			"transferOptions": commonOptions.Debug(transferOptions),
-		}).Debug("LOG")
-		r, err := regexp.Compile(watchOptions.FileNamePattern)
-		if err != nil {
+			"path":    transferOptions.InputPath,
+			"message": err.Error(),
+		}).Error("TRS")
+	}
+
+	if !watchOptions.Enabled {
+		walkfunc := func(inputPath string, f os.FileInfo, err error) error {
+			if !f.Mode().IsRegular() ||
+				!r.MatchString(inputPath) {
+				return nil
+			}
+			logrus.WithFields(logrus.Fields{
+				"file": inputPath,
+			}).Info("TRS")
+			if HandleTransfer(inputPath, serverOptions) != nil {
+				logrus.WithFields(logrus.Fields{
+					"file":    inputPath,
+					"message": err.Error(),
+				}).Error("TRS")
+			} else {
+				logrus.WithFields(logrus.Fields{
+					"file":    inputPath,
+					"message": "sent",
+				}).Info("TRS")
+			}
+			return nil
+		}
+		if err := filepath.Walk(transferOptions.InputPath, walkfunc); err != nil {
 			logrus.WithFields(logrus.Fields{
 				"path":    transferOptions.InputPath,
 				"message": err.Error(),
 			}).Error("TRS")
 		}
-		if !watchOptions.Enabled {
-			if err := filepath.Walk(
-				transferOptions.InputPath,
-				func(
-					inputPath string,
-					f os.FileInfo,
-					err error,
-				) error {
-					if !f.Mode().IsRegular() ||
-						!r.MatchString(inputPath) {
-						return nil
-					}
-					logrus.WithFields(logrus.Fields{
-						"file": inputPath,
-					}).Info("TRS")
-					if HandleTransfer(inputPath, serverOptions) != nil {
-						logrus.WithFields(logrus.Fields{
-							"file":    inputPath,
-							"message": err.Error(),
-						}).Error("TRS")
-					} else {
-						logrus.WithFields(logrus.Fields{
-							"file":    inputPath,
-							"message": "sent",
-						}).Info("TRS")
-					}
-					return nil
-				}); err != nil {
+		return
+	}
+
+	watchrecur.Watch(
+		transferOptions.InputPath,
+		watchOptions.Interval,
+		func(inputPath string) error {
+			if !r.MatchString(inputPath) {
+				return nil
+			}
+			if HandleTransfer(inputPath, serverOptions) != nil {
 				logrus.WithFields(logrus.Fields{
-					"path":    transferOptions.InputPath,
+					"file":    inputPath,
 					"message": err.Error(),
 				}).Error("TRS")
+			} else {
+				logrus.WithFields(logrus.Fields{
+					"file":    inputPath,
+					"message": "sent",
+				}).Info("TRS")
 			}
-			return
-		}
-		watchrecur.Watch(
-			transferOptions.InputPath,
-			watchOptions.Interval,
-			func(inputPath string) error {
-				if !r.MatchString(inputPath) {
-					return nil
-				}
-				if HandleTransfer(inputPath, serverOptions) != nil {
-					logrus.WithFields(logrus.Fields{
-						"file":    inputPath,
-						"message": err.Error(),
-					}).Error("TRS")
-				} else {
-					logrus.WithFields(logrus.Fields{
-						"file":    inputPath,
-						"message": "sent",
-					}).Info("TRS")
-				}
-				return nil
-			},
-		)
-	}
+			return nil
+		},
+	)
 }
 
 func HandleTransfer(
