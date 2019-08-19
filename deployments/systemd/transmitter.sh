e9354d423b2dcc8f0bb0fb8acd332dfcdba3554b
yangwu <yang.wu@igenetech.com>
2019-08-19 08:34:52 +0800

    Add deployment scripts

---
+++ deployments/systemd/transmitter.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+INPUT="/opt/automation/transmitter/input"
+cd /opt/automation/transmitter
+inotifywait -m -r -q $INPUT -e close_write --format '%w%f'|while read newfile
+do
+./transmitter \
+    -sourcepath=$newfile \
+    -namepattern=".\.abc$" \
+    -transfer=true \
+    -remotepath=/opt/automation/transmitter/test \
+    -hostkey="***REMOVED***" \
+    -username=igenetech \
+    -watch=true \
+    -password=***REMOVED***
+#-loglevel=debug \
+done
